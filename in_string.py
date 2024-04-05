b53403b6474ec35ed2a351c0618975d1407b20a4
auguscab <162723093+auguscab@users.noreply.github.com>
2024-04-05 11:38:24 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,5 +1,16 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    nombre= input("Ingrese su nombre: ")
+    a= "a" in nombre.lower()
+    e= "e" in nombre.lower()
+    i= "i" in nombre.lower()
+    o= "o" in nombre.lower()
+    u= "u" in nombre.lower()
+    print("contiene a: " + str(a))
+    print("contiene e: " + str(e))
+    print("contiene i: " + str(i))
+    print("contiene o: " + str(o))
+    print("contiene u: " + str(u))
 
 
 # Para verificar este ejercicio ejecutar el comando
