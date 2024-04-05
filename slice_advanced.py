2b80a53fd36732b3ad9fa74332a3d188aebfa715
auguscab <162723093+auguscab@users.noreply.github.com>
2024-04-05 12:05:25 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,7 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-    texto= "Awesome"
-    print(texto[0:3])
-    print(texto[2:5])
-    print(texto[0:4] + texto[-3:])
+    txt= input("Ingrese su texto: ")
+    print(txt[4::2])
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
